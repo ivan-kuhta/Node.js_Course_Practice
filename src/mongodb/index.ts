5a6c89ed935291b6fedcba1d105dc6dd26e91e19
Ivan <ivan.kuxta.gd@gmail.com>
2023-11-30 02:20:18 +0200

    add: added env variables

--- src/mongodb/index.ts
+++ src/mongodb/index.ts
@@ -1,6 +1,11 @@
+import { config } from 'dotenv'
 import { connect } from 'mongoose'
 
-const uri = 'mongodb+srv://ivan_kuxta:ivan_kuxta@learnnodejs.nlxd0u0.mongodb.net/?retryWrites=true&w=majority'
+if (process.env.NODE_ENV !== 'production') {
+  config()
+}
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@learnnodejs.nlxd0u0.mongodb.net/?retryWrites=true&w=majority`
 
 export async function connection(): Promise<void> {
   await connect(uri).then(() => { console.log('The database is connected') }).catch(e => { console.error(e) })
