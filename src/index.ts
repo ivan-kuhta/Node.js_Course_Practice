dfadfa3e3d2b072b8678223ee0c356ef2bfad70d
Ivan <ivan.kuxta.gd@gmail.com>
2023-12-22 16:46:29 +0200

    add: added constants

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 import express, { type Express } from 'express'
 
+import { HOST, PORT } from './constants'
 import router from './routes'
 import swagger from './swagger'
-import { connection } from './mongodb'
 
 const app: Express = express()
-
-const hostname: string = '127.0.0.1'
-const port: number = 3000
-
-app.use(express.json())
 
 app.use(swagger)
 
 app.use(router)
 
-app.listen(port, async () => {
-  await connection()
-  console.log(`Server is running on http://${hostname}:${port}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`)
 })
