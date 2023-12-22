279269a315f3c15fee20b11216d823b7ce4db72b
Ivan <ivan.kuxta.gd@gmail.com>
2023-12-22 16:46:29 +0200

    add: added constants

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import express, { Express } from 'express';
 
+import { HOST, PORT } from './constants';
 import router from './routes';
 import swagger from './swagger';
 
 const app: Express = express();
 
-const hostname: string = '127.0.0.1';
-const port: number = 3000;
-
 app.use(swagger);
 
 app.use(router)
 
-app.listen(port, () => {
-  console.log(`Server is running on http://${hostname}:${port}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`)
 })
